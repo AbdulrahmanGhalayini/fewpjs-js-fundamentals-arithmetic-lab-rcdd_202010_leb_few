3967f23870f4a27e49dce1d6b03225cec49a80fc
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-11-25 07:21:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function  mul (num1 , num2) {
 let multiply = mul(num1, num2);
 let mod = mode(num3, num4);
 
-
+let num3=24;
+let num4= 5;
 function  mode (num3 , num4) {
   if(num1%num2==4)
   return 4;
