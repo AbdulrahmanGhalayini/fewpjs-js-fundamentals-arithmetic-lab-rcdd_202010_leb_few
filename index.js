611c8da0f2d39da5c387268ad8e9abaddd41fe9d
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-11-25 07:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 let random = Math.floor(Math.random() * 16) + 5;
 let num1=1;
 let num2= 62;
-function ()
+function  mul (num1 , num2) {
+  if(num1*num2==62)
+  return 62;
+  return false;
+}
 let multiply = math.multiply(num1, num2);
 
 
